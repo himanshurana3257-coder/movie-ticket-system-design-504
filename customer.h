c5a844a42f62b005ad031a492b2a4d41d58dc9ca
HIMANSHU RANA <himanshurana3257@gmail.com>
2026-09-07 11:01:51 +0530

    Add Customer class definition in customer.h

---
+++ customer.h
@@ -0,0 +1,26 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include<string>
+
+using namespace std;
+
+
+class Customer
+{
+
+private:
+
+string name;
+
+string phone;
+
+
+public:
+
+Customer(string,string);
+
+};
+
+
+#endif
