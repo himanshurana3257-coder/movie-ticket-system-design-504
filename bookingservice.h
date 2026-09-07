c98d50619bee47192db5ebcb5971cfb0da854f26
HIMANSHU RANA <himanshurana3257@gmail.com>
2026-09-07 11:04:19 +0530

    Add BookingService class with createBooking method

---
+++ bookingservice.h
@@ -0,0 +1,25 @@
+#ifndef BOOKING_SERVICE_H
+#define BOOKING_SERVICE_H
+
+
+#include<vector>
+#include "Booking.h"
+
+
+class BookingService
+{
+
+private:
+
+vector<Booking> bookings;
+
+
+public:
+
+void createBooking(Booking);
+
+
+};
+
+
+#endif
