7553e93dbde52f3431bb3220afd2cc08ba89800d
HIMANSHU RANA <himanshurana3257@gmail.com>
2026-09-07 11:03:56 +0530

    Add Booking class with methods for ticket management

---
+++ booking.h
@@ -0,0 +1,25 @@
+#ifndef BOOKING_H
+#define BOOKING_H
+
+
+class Booking
+{
+
+private:
+
+int bookingId;
+
+double amount;
+
+
+public:
+
+void cancelBooking();
+
+void printTicket();
+
+
+};
+
+
+#endif
